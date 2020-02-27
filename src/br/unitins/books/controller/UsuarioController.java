65670e876dca21d935936ade2dddcd9615c2e7c4
aluno <aluno@172.16.91.117>
2020-02-27 10:27:34 -0300

    Implementando mais info do CRUD

--- src/br/unitins/books/controller/UsuarioController.java
+++ src/br/unitins/books/controller/UsuarioController.java
@@ -1,9 +1,13 @@
 package br.unitins.books.controller;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
+
+import org.apache.jasper.tagplugins.jstl.core.Remove;
 
 import br.unitins.books.model.Usuario;
 
@@ -13,10 +17,32 @@ public class UsuarioController implements Serializable{
 
 	private static final long serialVersionUID = -563227102506849534L;
 
+	//Pesquisar Cloneable
+	//Terminar os botoes
 	private Usuario usuario;
+	private List<Usuario> listaUsuario;
 	
 	public void incluir() {
-		System.out.println(getUsuario().getNome());
+		getListaUsuario().add(getUsuario());
+		limpar();
+	}
+	
+	public void limpar() {
+		usuario=null;
+	}
+	
+	
+	public void excluir() {
+		listaUsuario.remove(getUsuario());
+		limpar();
+	}
+	
+	public void alterar() {
+		
+	}
+	
+	public void editar(Usuario usu) {
+		setUsuario(usu);
 	}
 
 	public Usuario getUsuario() {
@@ -28,6 +54,11 @@ public class UsuarioController implements Serializable{
 	public void setUsuario(Usuario usuario) {
 		this.usuario = usuario;
 	}
-	
+
+	public List<Usuario> getListaUsuario() {
+		if(listaUsuario==null)
+			listaUsuario=new ArrayList<Usuario>();
+		return listaUsuario;
+	}
 	
 }
